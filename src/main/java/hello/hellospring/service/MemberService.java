aeea723d8eabcd05e3c4b5afb5d7f6fec0b73d0d
zzee22su <patrickstar0910@gmail.com>
2022-03-17 15:07:19 +0900

    IMP : Sign-up feature

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,63 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원가입
+     *
+     * -같은 이름이 있는 중복 회원X
+     */
+    public Long join(Member member) {
+//        -- Optional을 사용했으나 권장하지 않는 방법.
+//        Optional<Member> result = memberRepository.findByName(member.getName());
+//        result.ifPresent(m -> {
+//            throw new IllegalStateException("이미 존재하는 회원입니다.");
+//        });
+
+        /**
+         *  Optional을 사용하지 않았을 경우,
+         *  Member member1 = result.get(); 으로 값을 꺼내어
+         *  if(member1 != null)로 확인.
+         *  -> 권장하는 방법은 아니다.
+         */
+
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        //중복 회원 검증
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+        //Optional 사용으로 ifPresent를 사용할 수 있다.
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+
+}

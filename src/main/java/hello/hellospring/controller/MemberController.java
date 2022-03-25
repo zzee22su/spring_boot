dc420cd1f46ce05d930facf0d52c2991800d0202
zzee22su <patrickstar0910@gmail.com>
2022-03-25 11:10:41 +0900

    ADD : 회원가입, 회원목록 매핑

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -19,4 +25,27 @@ public class MemberController {
     //MemberService를 spring이 spring 컨테이너에 있는 memberservice를 가져다가 연결을 해준다.
 
 //    MemberController가 등록될 때 Spring Bean에 등록되어 있는 MemberService 객체를 넣어준다. => Dependency Injection
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
 }
